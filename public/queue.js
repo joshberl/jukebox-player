edbb368d05b79baaa9202bf5986612760212ff30
David McConnell <david.mcconnell@tufts.edu>
2015-12-14 22:45:19 -0500

    fixing file errors from git

--- public/queue.js
+++ public/queue.js
@@ -109,37 +109,3 @@ var load_player = function(data) {
 	var iframe = "<iframe src=\"https://embed.spotify.com/?uri=" + data.queue[0].uri + "\" frameborder=\"0\" allowtransparency=\"true\"></iframe>";
 	player.innerHTML += iframe;
 }
-
-// var searchsong = function(req, res) {
-// 	var song = req.query.song;
-// 	if (song != "") {
-// 		spotify.search({ type: 'track', query: song }, function(err, data) {
-// 			if (!err) {
-// 				var text = "";
-// 				var queuesongs = [];
-// 			    for (x in data['tracks']['items']) {
-// 			    	var queuetitle = data['tracks']['items'][x]['name'];
-// 			    	var queueartist = data['tracks']['items'][x]['artists'][0]['name'];
-// 			    	var queueobj = {"title": queuetitle, "artist": queueartist}
-// 			    	queuesongs.push(queueobj);
-// 			    	text += data['tracks']['items'][x]['name'] + " – " + data['tracks']['items'][x]['artists'][0]['name'] + "<br>"; 
-// 			    	// console.log(data['tracks']['items'][x]['name']);
-// 			    	// console.log(data['tracks']['items'][x]['album']['name']);
-// 			    	// console.log(data['tracks']['items'][x]['artists'][0]['name']);
-// 			    	// i++;
-// 			    }
-// 			    console.log(queuesongs);
-// 			    res.status(200);
-// 			    res.send(text);
-// 			}
-// 			else {
-// 				res.status(200);
-// 				res.send("An error occurred, please try again");
-// 			}
-// 		});
-// 	}
-// 	else {
-// 		res.status(200);
-// 		res.send("Please enter something to search");
-// 	}
-// };
