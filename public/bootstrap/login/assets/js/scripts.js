34236c9304d30435e5eef7b1e7a3e12a64ef1c9c
David McConnell <david.mcconnell@tufts.edu>
2015-12-15 23:17:51 -0500

    style

--- public/bootstrap/login/assets/js/scripts.js
+++ public/bootstrap/login/assets/js/scripts.js
@@ -1,31 +1,11 @@
 
 jQuery(document).ready(function() {
-	var isMobile = {
-		Android: function() {
-		    return navigator.userAgent.match(/Android/i);
-		},
-		BlackBerry: function() {
-		    return navigator.userAgent.match(/BlackBerry/i);
-		},
-		iOS: function() {
-		    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-		Opera: function() {
-		    return navigator.userAgent.match(/Opera Mini/i);
-		},
-		Windows: function() {
-		    return navigator.userAgent.match(/IEMobile/i);
-		},
-		any: function() {
-		    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
-    
-    if (!isMobile.any) {
+    if (screen.width >= 480) {
     	$.backstretch("bootstrap/login/assets/img/backgrounds/Homepage_bg.jpg");
     }
     else {
     	document.body.style.backgroundImage = "url('/../bootstrap/login/assets/img/backgrounds/Homepage_bg.jpg')";
+    	document.body.style.backgroundRepeat = "repeat-x";
     }
 
 });
